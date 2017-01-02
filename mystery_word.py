2508ec7fc3bdf09357b010a255696e8351fca8ba
John D. Bower <jdbower0525@gmail.com>
2017-01-02 11:23:06 -0500

    final commit

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,6 @@
 import random
-import sys
 import os
+
 
 def clear():
     if os.name == 'nt':
@@ -8,22 +8,27 @@ def clear():
     else:
         os.system('clear')
 
-def random_word():
+easy_list = []
+normal_list = []
+hard_list = []
+
+
+def organize_words():
     print('')
-    easy_list = []
-    normal_list = []
-    hard_list = []
     with open("/usr/share/dict/words", "r") as f:
         for line in f:
             words = line.lower().split()
             for word in words:
                 if len(word) > 3 and len(word) <= 6:
                     easy_list.append(word)
-                elif len(word) >= 6 and len(word) <= 8:
+                elif len(word) > 6 and len(word) <= 8:
                     normal_list.append(word)
                 elif len(word) > 8:
                     hard_list.append(word)
-    print("Easy = 4-6 letters, Normal = 6-8 letters, and Hard is more than 8 letters!")
+
+def choose_difficulty():
+    print("Easy = 4-6 letters, Normal = 6-8 letters, "
+          "and Hard is more than 8 letters!")
     difficulty = input("Choose a difficulty! (Easy/Normal/Hard) ").lower()
     if difficulty[0] != "e" and difficulty[0] != "n" and difficulty[0] != "h":
         print("TYPE EASY, NORMAL, OR HARD TO CHOOSE DIFFICULTY")
@@ -37,6 +42,7 @@ def random_word():
     elif difficulty[0] == "h":
         rand_word = random.choice(hard_list)
         return rand_word
+
 
 def user_guess(bad_guesses, good_guesses):
     while len(bad_guesses) < 9:
@@ -55,6 +61,7 @@ def user_guess(bad_guesses, good_guesses):
             continue
         return guess
 
+
 def comparison(new_guess, rand_word, bad_guesses, good_guesses):
     if new_guess in rand_word:
         good_guesses.append(new_guess)
@@ -62,6 +69,7 @@ def comparison(new_guess, rand_word, bad_guesses, good_guesses):
     elif new_guess not in rand_word:
         bad_guesses.append(new_guess)
         return bad_guesses
+
 
 def display(rand_word, good_guesses, bad_guesses, new_guess):
     clear()
@@ -88,17 +96,20 @@ def display(rand_word, good_guesses, bad_guesses, new_guess):
     if remaining_letters == 0:
         win_condition(rand_word, bad_guesses)
 
+
 def win_condition(rand_word, bad_guesses):
     clear()
     print("\nCONGRATULATIONS!!! The secret word was {}.".format(rand_word))
     print("You only guessed wrong {} times!".format(len(bad_guesses)))
     game_reset()
 
+
 def loss_condition(rand_word):
     clear()
     print("\nOH NO! You ran out of guesses!")
     print("The secret word was {}.".format(rand_word))
     game_reset()
+
 
 def game_reset():
     replay = input("Would you like to play again? (Yes/No) ").lower()
@@ -109,10 +120,13 @@ def game_reset():
     else:
         exit()
 
+
 def main():
     good_guesses = []
     bad_guesses = []
-    rand_word = random_word()
+    if len(easy_list)<1:
+        organize_words()
+    rand_word = choose_difficulty()
     while len(bad_guesses) < 9:
         new_guess = user_guess(bad_guesses, good_guesses)
         comparison(new_guess, rand_word, bad_guesses, good_guesses)
--- mystery_word_test.py
+++ mystery_word_test.py
@@ -1,37 +1,34 @@
+import random
 import unittest
-from mystery_word import random_word
-from mystery_word import game_reset
-from mystery_word import loss_condition
+from mystery_word import *
 
 
+class TestMysteryWordMethods(unittest.TestCase):
+    def test_organize_words(self):
+        organize_words()
+        self.assertTrue(len(random.choice(easy_list)) <= 6)
+        self.assertTrue(len(random.choice(easy_list)) > 3)
+        self.assertTrue(len(random.choice(normal_list)) > 6)
+        self.assertTrue(len(random.choice(normal_list)) <= 8)
+        self.assertTrue(len(random.choice(hard_list)) > 8)
 
-class TestMysteryWordFunctions(unittest.TestCase):
+    def test_good_guess(self):
+        good_guesses = []
+        bad_guesses = []
+        new_guess = 'a'
+        rand_word = 'apple'
+        comparison(new_guess, rand_word, bad_guesses, good_guesses)
+        self.assertTrue(len(good_guesses) > 0)
+        self.assertTrue(len(bad_guesses) == 0)
 
-    def test_game_reset(self):
-        user_input_1 = False
-        self.assertRaises(SystemExit, user_input_1)
-        user_input_2 = True
-        self.assertEqual(main(), user_input_2)
-
-    def test_loss_condition(self):
-        self.return_value = "random"
-        self.side_effect = list("bcdefgh")
-        mystery_word.main()
-        self.xprint.assert_any_call("The secret word was random")
-
-    def test_random_word(self):
-       user_diff_choice_one = 'e'
-       user_diff_choice_two = 'm'
-       user_diff_choice_three = 'h'
-       easy = ['easy']
-       medium = ['mediums']
-       hard = ['difficults']
-       random_word_one = random_word()
-       random_word_two = random_word()
-       random_word_three = random_word(user_diff_choice_three, easy, medium, hard)
-       self.assertEqual(random_word_one, 'easy')
-       self.assertEqual(random_word_two, 'mediums')
-       self.assertEqual(random_word_three, 'difficults')
+    def test_bad_guess(self):
+        good_guesses = []
+        bad_guesses = []
+        new_guess = 'a'
+        rand_word = 'blueberry'
+        comparison(new_guess, rand_word, bad_guesses, good_guesses)
+        self.assertTrue(len(good_guesses) == 0)
+        self.assertTrue(len(bad_guesses) > 0)
 
 if __name__ == '__main__':
     unittest.main()
